56eabc4c84a39fbf1979d075e115b0a242076319
santhiyababu <santhiya.laya@gmail.com>
2016-06-07 15:02:31 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,12 +1,26 @@
 #include<stdio.h>
 #include<conio.h>
-int main()
+int main() 
 {
-    char str[50];
-    char *rev;
-    printf("Enter any string : ");
-    scanf("%s",str);
-    rev = strrev(str);
-    printf("Reverse string is : %s",rev);
-    return 0;
+   char str[100], temp;
+   int i, j = 0;
+ 
+   printf("\nEnter the string :");
+   gets(str);
+ 
+   i = 0;
+   j = strlen(str) - 1;
+ 
+   while (i < j) 
+   {
+      temp = str[i];
+      str[i] = str[j];
+      str[j] = temp;
+      i++;
+      j--;
+   }
+ 
+   printf("\nReverse string is :%s", str);
+   return (0);
 }
+
